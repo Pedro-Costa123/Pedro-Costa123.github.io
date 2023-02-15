46d6f7f7edd4816f454c802f2f31e71a3b8cee05
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-02-15 18:23:19 +0000

    Configure Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
+        <p>Pedro Costa Portfolio Website using React</p>
+        <p className={styles.AppText}>Work In Progress</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
