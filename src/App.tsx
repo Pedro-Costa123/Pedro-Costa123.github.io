c21718d6ae8ba1ef7eecbb1c40fbf66a244b2622
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-02-21 17:38:58 +0000

    Added GitHub and LinkedIn logos and urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import logo from "./logo.svg";
+import logo from "./assets/logo.svg";
+import githubLogo from "./assets/github.svg";
+import linkedInLogo from "./assets/linkedin.svg";
 import styles from "./App.module.css";
 
 const App = () => {
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
+    <div className={styles.app}>
+      <main className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>Pedro Costa Portfolio Website using React</p>
-        <p className={styles.AppText}>Work In Progress</p>
-      </header>
+        <p className={styles.appText}>Work In Progress</p>
+        <div className={styles.social_icons}>
+          <a
+            href="https://www.linkedin.com/in/pedro-costa-a2173213b/"
+            className={styles.social_icon}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedInLogo} alt="LinkedIn Profile" />
+          </a>
+          <a
+            href="https://github.com/Pedro-Costa123"
+            className={styles.social_icon}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubLogo} alt="GitHub Profile" />
+          </a>
+        </div>
+      </main>
     </div>
   );
 };
