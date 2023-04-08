22324d52975e11e1ec1a311a67577f828e36e3b1
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-04-08 17:39:46 +0100

    Contact Form Validation

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -6,9 +6,35 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
+  const [subjectValid, setSubjectValid] = useState(false);
+  const [messageValid, setMessageValid] = useState(false);
 
   const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailValid(true);
+      return;
+    } else {
+      setEmailValid(false);
+    }
+
+    // Check if subject is not empty
+    if (!subject.trim()) {
+      setSubjectValid(true);
+      return;
+    } else {
+      setSubjectValid(false);
+    }
+
+    // Check if message is not empty
+    if (!message.trim()) {
+      setMessageValid(true);
+      return;
+    } else {
+      setMessageValid(false);
+    }
 
     window.open(`mailto:${email}?subject=${subject}&body=${message}`);
     //after replace by formspree -> https://formspree.io/
@@ -21,7 +47,7 @@ const Contact = () => {
   return (
     <>
       <h4 className={classes.contentTitle}>Contact</h4>
-      <form className={classes.contactForm} onSubmit={handleSubmit}>
+      <form className={classes.contactForm} onSubmit={handleSubmit} noValidate>
         <label className={classes.label} htmlFor="email">
           Email:
         </label>
@@ -34,6 +60,9 @@ const Contact = () => {
           onChange={(event) => setEmail(event.target.value)}
           required
         />
+        {emailValid && (
+          <p className={classes.error}>Please enter a valid email address</p>
+        )}
         <label className={classes.label} htmlFor="subject">
           Subject:
         </label>
@@ -46,6 +75,9 @@ const Contact = () => {
           onChange={(event) => setSubject(event.target.value)}
           required
         />
+        {subjectValid && (
+          <p className={classes.error}>Subject cannot be empty</p>
+        )}
         <label className={classes.label} htmlFor="message">
           Message:
         </label>
@@ -57,6 +89,9 @@ const Contact = () => {
           onChange={(event) => setMessage(event.target.value)}
           required
         ></textarea>
+        {messageValid && (
+          <p className={classes.error}>Message cannot be empty</p>
+        )}
         <input className={classes.submitButton} type="submit" value="Submit" />
       </form>
     </>
