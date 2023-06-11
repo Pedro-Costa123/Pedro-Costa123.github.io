d85e86ca10120f16adc33ff82350d85f8a3d37e3
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-06-11 17:32:26 +0100

    Added more certifications & URL to them

--- src/components/pages/education/Certifications.tsx
+++ src/components/pages/education/Certifications.tsx
@@ -8,21 +8,69 @@ const Certifications = () => {
         <ul>
           <li>
             <p className={classes.titleCert}>
-              React - The Complete Guide (incl Hooks, React Router, Redux) (48.5
-              hours)
+              <a
+                href="https://www.credly.com/badges/f0bf0ccb-9c40-45ec-9866-45f16e6c345a/linked_in_profile"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Professional Scrum Master™ I (PSM I)
+              </a>
+            </p>
+            <p className={classes.dateCert}>May - 2023</p>
+          </li>
+          <li>
+            <p className={classes.titleCert}>
+              EF Level 16 - Upper Advanced - CEFR Level C2
+            </p>
+            <p className={classes.dateCert}>May - 2023</p>
+          </li>
+          <li>
+            <p className={classes.titleCert}>
+              <a
+                href="https://www.udemy.com/certificate/UC-f8fd054a-7272-4129-aeaa-4ffc5275fb00/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                The Complete C Programming Bootcamp (25 hours)
+              </a>
+            </p>
+            <p className={classes.dateCert}>May - 2023</p>
+          </li>
+          <li>
+            <p className={classes.titleCert}>
+              <a
+                href="https://www.udemy.com/certificate/UC-8c283f13-953c-4daa-9d30-2519e188d4a3/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                React - The Complete Guide (incl Hooks, React Router, Redux)
+                (48.5 hours)
+              </a>
             </p>
             <p className={classes.dateCert}>Feb - 2023</p>
           </li>
           <li className={classes.liCert}>
             <p className={classes.titleCert}>
-              Unreal Engine 5 C++ Developer: Learn C++ & Make Video Games (29.5
-              hours)
+              <a
+                href="https://www.udemy.com/certificate/UC-3161e477-aab4-4bcc-a460-7358622b1acf/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Unreal Engine 5 C++ Developer: Learn C++ & Make Video Games
+                (29.5 hours)
+              </a>
             </p>
             <p className={classes.dateCert}>Nov - 2022</p>
           </li>
           <li className={classes.liCert}>
             <p className={classes.titleCert}>
-              Flutter & Dart - The Complete Guide [2022 Edition] (42.5 hours)
+              <a
+                href="https://www.udemy.com/certificate/UC-8a62570b-fda9-4f77-811f-c62d28b8ef51/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Flutter & Dart - The Complete Guide [2022 Edition] (42.5 hours)
+              </a>
             </p>
             <p className={classes.dateCert}>Oct - 2022</p>
           </li>
