0272a63bf78a92cdf3970132b4a04406fedd5f04
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-10-03 12:18:11 +0100

    Added photo to about me

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,28 +1,38 @@
+import photo from "../../assets/PedroCosta.jpeg";
 import classes from "./About.module.css";
 
 const About = () => {
   return (
     <>
       <div className={classes.about}>
-        <h4 className={classes.contentTitle}>About Me</h4>
-        <p className={classes.justify}>
-          Bachelor's degree in Computer Science and Engineering.
-        </p>
-        <p className={classes.justify}>
-          I have a diverse skill set that includes Java, Kotlin, Python,
-          JavaScript, TypeScript, Dart, and frameworks like Flutter, Ionic,
-          Angular, and React.js. I am passionate about creating innovative
-          solutions and leveraging technology to solve complex problems.
-        </p>
-        <p className={classes.justify}>
-          With my knowledge and skills, I am confident in my ability to take on
-          challenging projects and deliver high-quality results. I am also a
-          quick learner, constantly seeking new knowledge and skills to improve
-          my abilities.
-        </p>
-        <p className={classes.justify}>
-          Currently working at Capgemini as a Java Software Engineer.
-        </p>
+        <div className={classes.imageContainer}>
+          <img
+            className="rounded-circle shadow-lg"
+            src={photo}
+            alt="Pedro Costa"
+          />
+        </div>
+        <div className={classes.textContainer}>
+          <h4 className={classes.contentTitle}>About Me</h4>
+          <p className={classes.justify}>
+            Bachelor's degree in Computer Science and Engineering.
+          </p>
+          <p className={classes.justify}>
+            I have a diverse skill set that includes Java, Kotlin, Python,
+            JavaScript, TypeScript, Dart, and frameworks like Flutter, Ionic,
+            Angular, and React.js. I am passionate about creating innovative
+            solutions and leveraging technology to solve complex problems.
+          </p>
+          <p className={classes.justify}>
+            With my knowledge and skills, I am confident in my ability to take
+            on challenging projects and deliver high-quality results. I am also
+            a quick learner, constantly seeking new knowledge and skills to
+            improve my abilities.
+          </p>
+          <p className={classes.justify}>
+            Currently working at Capgemini as a Java Software Engineer.
+          </p>
+        </div>
       </div>
     </>
   );
