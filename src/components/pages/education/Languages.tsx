65c599c9644a20461f1a553a72abcd5cd8f49b52
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-10-25 10:24:45 +0100

    Languages data is now in a json

--- src/components/pages/education/Languages.tsx
+++ src/components/pages/education/Languages.tsx
@@ -1,40 +1,37 @@
+import { useEffect, useState } from "react";
 import classes from "./Languages.module.css";
+import Language from "../../../models/language";
 
 const Languages = () => {
+  const [languages, setLanguages] = useState([] as Language[]);
+
+  useEffect(() => {
+    fetch("data/languages.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setLanguages(data.languages);
+      });
+  }, []);
+
   return (
     <>
       <h4 className={classes.contentTitle}>Languages</h4>
       <div className={classes.languages}>
-        <div>
-          <div className={classes.language}>
-            <p>English</p>
-            <div className={classes.proficiency}>
-              <span className={classes.bar} style={{ width: "90%" }}>
-                90%
-              </span>
+        {languages.map((language) => (
+          <div key={language.name}>
+            <div className={classes.language}>
+              <p>{language.name}</p>
+              <div className={classes.proficiency}>
+                <span
+                  className={classes.bar}
+                  style={{ width: `${language.proficiency}%` }}
+                >
+                  {language.proficiency}%
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div className={classes.language}>
-            <p>Portuguese</p>
-            <div className={classes.proficiency}>
-              <span className={classes.bar} style={{ width: "100%" }}>
-                100%
-              </span>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className={classes.language}>
-            <p>Spanish</p>
-            <div className={classes.proficiency}>
-              <span className={classes.bar} style={{ width: "50%" }}>
-                50%
-              </span>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
---
+++ src/models/language.ts
@@ -0,0 +1,11 @@
+class Language {
+  name: string;
+  proficiency: number;
+
+  constructor(name: string, proficiency: number) {
+    this.name = name;
+    this.proficiency = proficiency;
+  }
+}
+
+export default Language;
