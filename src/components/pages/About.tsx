870e690b7ef3ce366c7ab31b0140442f0553f598
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-10-26 15:07:59 +0100

    About data is now in a json

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import photo from "../../assets/PedroCosta.jpeg";
 import classes from "./About.module.css";
 
 const About = () => {
+  const [about, setAbout] = useState([] as string[]);
+  useEffect(() => {
+    fetch("data/about.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAbout(data.about);
+      });
+  }, []);
+
   return (
     <>
       <div className={classes.about}>
@@ -14,24 +24,11 @@ const About = () => {
         </div>
         <div className={classes.textContainer}>
           <h4 className={classes.contentTitle}>About Me</h4>
-          <p className={classes.justify}>
-            Bachelor's degree in Computer Science and Engineering.
-          </p>
-          <p className={classes.justify}>
-            I have a diverse skill set that includes Java, Kotlin, Python,
-            JavaScript, TypeScript, Dart, and frameworks like Flutter, Ionic,
-            Angular, and React.js. I am passionate about creating innovative
-            solutions and leveraging technology to solve complex problems.
-          </p>
-          <p className={classes.justify}>
-            With my knowledge and skills, I am confident in my ability to take
-            on challenging projects and deliver high-quality results. I am also
-            a quick learner, constantly seeking new knowledge and skills to
-            improve my abilities.
-          </p>
-          <p className={classes.justify}>
-            Currently working at Capgemini as a Java Software Engineer.
-          </p>
+          {about.map((paragraph, index) => (
+            <p className={classes.justify} key={index}>
+              {paragraph}
+            </p>
+          ))}
         </div>
       </div>
     </>
