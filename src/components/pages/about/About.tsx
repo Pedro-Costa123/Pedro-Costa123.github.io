6d569a6447264b13e6ebe869f5237714543e8d88
Pedro Costa <102796961+Pedro-Costa123@users.noreply.github.com>
2026-02-27 12:42:21 +0000

    Restructure About component layout and enhance styling

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -64,21 +64,26 @@ const About = () => {
   }
 
   return (
-    <>
-      <div className={classes.about}>
-        <div className={classes.imageContainer}>
-          <img className={classes.image} src={photo} alt="Pedro Costa" />
-        </div>
-        <div className={classes.textContainer}>
-          <h4 className={classes.contentTitle}>About Me</h4>
+    <div className={classes.about}>
+      <figure className={classes.imageContainer}>
+        <img className={classes.image} src={photo} alt="Pedro Costa" />
+      </figure>
+      <div className={classes.textContainer}>
+        <h4 className={classes.contentTitle}>About Me</h4>
+        <div className={classes.copy}>
           {about.map((paragraph, index) => (
-            <p className={classes.justify} key={index}>
+            <p
+              className={`${classes.justify} ${
+                index === 0 ? classes.leadParagraph : ""
+              }`}
+              key={index}
+            >
               {paragraph}
             </p>
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
